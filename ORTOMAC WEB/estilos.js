593c204b3f99ef964876c42bc0573ae95f7265ea
Your Name <you@example.com>
2023-01-24 11:43:15 -0500

    primer comentario

---
+++ ORTOMAC WEB/estilos.js
@@ -0,0 +1,25 @@
+/*MENU DE NAVEGACIÓN*/
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+/*Galeria*/
+
+
+
